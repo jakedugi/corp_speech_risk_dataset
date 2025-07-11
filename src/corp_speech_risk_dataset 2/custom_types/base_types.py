7a8770b0d36858415dfcf211fe8b0e7c71809a5e
jakedugi <jake@jakedugan.com>
2025-07-11 15:21:53 +0100

    Clean up duplicated files. Due to ICloud Sync.

--- src/corp_speech_risk_dataset 2/custom_types/base_types.py
+++
@@ -1,6 +0,0 @@
-from typing import Optional
-
-class APIConfig:
-    def __init__(self, api_key: Optional[str] = None, rate_limit: float = 0.25):
-        self.api_key = api_key
-        self.rate_limit = rate_limit
